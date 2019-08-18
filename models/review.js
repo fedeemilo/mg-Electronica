04f8250003aca08ea04cf3b358acaa219e889cc0
fedeemilo <fedeemilo91@gmail.com>
2019-08-18 13:58:24 -0300

    adding review model

--- models/review.js
+++ models/review.js
@@ -1,3 +1,17 @@
-// Review
-// -body -string
-// -author -object id (ref User)
+
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+const Schema = mongoose.Schema;
+
+const ReviewSchema = new Schema({
+   body: String,
+   author: {
+       type: Schema.Types.ObjectId,
+       ref: 'User'
+   }
+});
+
+UserSchema.plugin(passportLocalMongoose)
+
+module.exports = mongoose.model('Review', ReviewSChema);
+
