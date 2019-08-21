aa48efdf2ecd0a3517b06aebf6250ccf119baae8
fedeemilo <fedeemilo91@gmail.com>
2019-08-21 01:56:12 -0300

    add multiple image upload feature to post resource

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
@@ -5,10 +7,10 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const passport = require('passport');
-const passportLocalMongoose = require('passport-local-mongoose');
 const User = require('./models/user');
 const session = require('express-session');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 
 
 // require routes
@@ -31,10 +33,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
 
 // Configure Passport and Session
 app.set('trust proxy', 1) // trust first proxy
--- controllers/posts.js
+++ controllers/posts.js
@@ -1,26 +1,57 @@
 const Post = require('../models/post');
+const cloudinary = require('cloudinary');
+cloudinary.config({
+    cloud_name: 'mg-electronica',
+    api_key: '811337271329754',
+    api_secret: process.env.CLOUDINARY_SECRET
+});
 
 module.exports = {
     //POSTS INDEX
-    async getPosts(req, res, next) {
+    async postIndex(req, res, next) {
         let posts = await Post.find({});
         res.render('posts/index', { posts: posts });
     },
     //POSTS NEW
-    newPost(req, res, next) {
+    postNew(req, res, next) {
         res.render('posts/new');
     },
 
     //POSTS CREATE
-    async createPost(req, res, next) {
-        // use req.body to create a new Post
-        let post = await Post.create(req.body);
+    async postCreate(req, res, next) {
+        req.body.post.images = [];
+        for (const file of req.files) {
+            let image = await cloudinary.v2.uploader.upload(file.path);
+            req.body.post.images.push({
+                url: image.secure_url,
+                public_id: image.public_id
+            });
+        }
+        let post = await Post.create(req.body.post);
         res.redirect(`/posts/${post.id}`);
     },
 
    //POSTS SHOW
-   async showPost(req, res, next) {
+   async postShow(req, res, next) {
        let post = await Post.findById(req.params.id);
        res.render('./posts/show', { post })
+   },
+
+   //POSTS EDIT
+   async postEdit(req, res, next) {
+       let post = await Post.findById(req.params.id);
+       res.render('posts/edit', { post });
+   },
+
+   //POSTS UPDATE
+   async postUpdate(req, res, next){
+       let post = await Post.findByIdAndUpdate(req.params.id, req.body.post, { new: true });
+       res.redirect(`/posts/${post.id}`);
+   },
+
+   //POSTS DESTROY
+   async postDestroy(req, res, next){
+       let post = await Post.findByIdAndRemove(req.params.id);
+       res.redirect('/posts');
    }
 }
